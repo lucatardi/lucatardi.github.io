7d1a73c5572ab4f8738edaca37c5e5465f0d1131
Luca tardito <lucatardi@gmail.com>
2018-03-03 15:53:59 +0000

    define js file

---
+++ app.js
@@ -0,0 +1,5 @@
+const buttonOpenVideoPlayer = document.getElementById("open-video-player");
+const buttonCodeVideoPlayer = document.getElementById("code-video-player");
+
+buttonOpenVideoPlayer.addEventListener("click",() => window.open('http://www.lucatardito.com/Interactive-Video-Player','_blank','resizable=yes'));
+buttonCodeVideoPlayer.addEventListener("click",() => window.open('https://github.com/lucatardi/Interactive-Video-Player','_blank','resizable=yes'));
